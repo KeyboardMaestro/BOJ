8b4e33c23f099885b36c8ecd5098b6c761427940
Sungwon Jude Park <psw0616a@naver.com>
2023-07-03 19:58:23 +0900

    fix monor bugs

--- Java/Q1152.java
+++ Java/Q1152.java
@@ -2,8 +2,17 @@ import java.util.Scanner;
 
 public class Q1152 {
     public static void main(String[] args){
+        int words = 1;
         Scanner scanner = new Scanner(System.in);
-        String str = scanner.nextLine();
-        System.out.println(str.trim().split(" ").length);
+        String str = scanner.nextLine().trim();
+        if(str.equals(" ") || str.isEmpty()){
+            System.out.println(0);
+            return;
+        }
+        for (int i = 0; i < str.length(); i++){
+            if(str.charAt(i) == ' ')
+                words++;
+        }
+        System.out.println(words);
     }
 }
--- Java/tempCodeRunnerFile.java
+++ Java/tempCodeRunnerFile.java
@@ -1,2 +1,18 @@
+import java.util.Scanner;
 
+public class Q1152 {
+    public static void main(String[] args){
+        int words = 1;
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.nextLine().trim();
+        if(str.equals(" ")){
+            System.out.println(0);
+            return;
+        }
+        for (int i = 0; i < str.length(); i++){
+            if(str.charAt(i) == ' ')
+                words++;
+        }
+        System.out.println(words);
     }
+}
